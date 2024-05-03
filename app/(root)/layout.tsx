335bea7b5777394625821d806f8bf7f87f834d1c
andreitotoro <andreitotoro@gmail.com>
2024-05-03 13:54:37 +0300

    Containerized the app and created demo navbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,11 +6,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
+    <div className="p-5 max-w-[1600px] mx-auto">
       <Navbar />
       <div className="flex-center min-h-screen w-full bg-primary-50 bg-dotted-pattern bg-cover bg-fixed bg-center">
         {children}
       </div>
-    </>
+    </div>
   );
 }
--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -5,7 +5,8 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div>
+    <div className="flex justify-between">
+      <h1 className="font-black text-2xl">Atomic To-Do</h1>
       <SignedIn>
         <UserButton />
       </SignedIn>
