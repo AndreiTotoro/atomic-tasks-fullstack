b21988de29f7a91870a9e740242f1bbf61c7d101
andreitotoro <andreitotoro@gmail.com>
2024-05-03 14:29:51 +0300

    Create add new task form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,12 @@
+import { CreateTaskForm } from "@/components/shared/CreateTaskForm";
 import { createUser } from "@/lib/actions/user.actions";
 import { CreateUserParams } from "@/types";
 import React, { useEffect } from "react";
 
 export default async function page() {
-  return <div className="text-violet-500">Hi</div>;
+  return (
+    <div className="text-violet-500">
+      <CreateTaskForm />
+    </div>
+  );
 }
---
+++ components/shared/CreateTaskForm.tsx
@@ -0,0 +1,119 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Checkbox } from "../ui/checkbox";
+
+const formSchema = z.object({
+  taskName: z
+    .string()
+    .min(2, { message: "Task name must be at least 2 characters long." }),
+  taskDescription: z.string().min(2, {
+    message: "Task description must be at least 2 characters long.",
+  }),
+  isTaskOfTheDay: z.boolean(),
+});
+
+export function CreateTaskForm() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      taskName: "",
+      taskDescription: "",
+      isTaskOfTheDay: false,
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <div className="max-w-96 mx-auto mt-12 bg-neutral-500/20 p-5 rounded-md">
+      <h1 className="text-white text-center font-bold text-xl">
+        Create new task
+      </h1>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 pt-5"
+        >
+          <FormField
+            control={form.control}
+            name="taskName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Task Name</FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    placeholder="task name"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>Give your task a description.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="taskDescription"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Task Description</FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    placeholder="task description"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>Give your task a name..</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="isTaskOfTheDay"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Is task of the day?</FormLabel>
+                <FormControl>
+                  <div className="flex items-center space-x-2">
+                    <Checkbox
+                      id="isTaskOfTheDay"
+                      checked={field.value}
+                      className="mr-2 h-5 w-5 border-2 border-primary-500"
+                      onCheckedChange={field.onChange}
+                    />
+                  </div>
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
