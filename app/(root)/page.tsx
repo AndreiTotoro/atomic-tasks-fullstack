dc19a7ea3e18f679fb53b6042861cd23184a7cd5
andreitotoro <andreitotoro@gmail.com>
2024-05-03 13:36:53 +0300

    Sucessfully set up clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,5 @@
+import { createUser } from "@/lib/actions/user.actions";
+import { CreateUserParams } from "@/types";
 import React, { useEffect } from "react";
 
 export default async function page() {
