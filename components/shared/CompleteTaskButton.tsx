4c7ed6868f1d509c9c41aef9d8d928cbe20cec6c
andreitotoro <andreitotoro@gmail.com>
2024-05-04 23:28:21 +0300

    Add loading animations to the button being clicked

--- components/shared/CompleteTaskButton.tsx
+++ components/shared/CompleteTaskButton.tsx
@@ -1,16 +1,26 @@
 "use client";
-import { setTaskCompleted } from "@/lib/actions/task.actions";
-import React from "react";
-import { Button } from "../ui/button";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { setTaskCompleted } from "@/lib/actions/task.actions";
+import { Button } from "../ui/button";
 
 export default function CompleteTaskButton({ taskId }: { taskId: string }) {
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
 
   const handleClick = async () => {
+    setIsLoading(true);
     await setTaskCompleted(taskId);
+    setIsLoading(false);
     router.refresh();
   };
 
-  return <Button onClick={() => handleClick()}>Complete</Button>;
+  return (
+    <Button
+      onClick={handleClick}
+      disabled={isLoading}
+    >
+      {isLoading ? "Loading..." : "Complete"}
+    </Button>
+  );
 }
