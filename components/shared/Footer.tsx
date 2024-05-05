de83d5b5a8416c68304ba87393405cbe02570226
andreitotoro <andreitotoro@gmail.com>
2024-05-05 16:22:09 +0300

    Change footer look

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -2,9 +2,11 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="flex justify-center items-center h-16 pt-5 text-white">
+    <footer className="flex flex-col justify-center items-center h-16 pt-5 text-white">
+      <p>© 2024 Atomic Tasks</p>
+      <br />
       <p>
-        © 2024 Atomic Tasks - Created with ❤️ by <b>AndreiTotoro</b>
+        Created with ❤️ by <b>AndreiTotoro</b>
       </p>
     </footer>
   );
